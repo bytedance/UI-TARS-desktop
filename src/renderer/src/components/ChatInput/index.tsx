88e88cead0295a2334333784f82c53dc6f285dd5
jinxin001 <jinxin001@bytedance.com>
2025-01-21 11:10:15 +0800

    docs: license

--- src/renderer/src/components/ChatInput/index.tsx
+++ src/renderer/src/components/ChatInput/index.tsx
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 Bytedance Ltd. and/or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { Box, Button, Flex, HStack, Spinner, VStack } from '@chakra-ui/react';
 import React, { forwardRef, useEffect, useRef } from 'react';
 import { FaPaperPlane, FaStop, FaTrash } from 'react-icons/fa';
