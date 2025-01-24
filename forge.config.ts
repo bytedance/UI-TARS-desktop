def9b62491226e92772930b4cde46f8da0b6797d
jinxin001 <jinxin001@bytedance.com>
2025-01-24 11:27:29 +0800

    chore: hardenedRuntime

--- forge.config.ts
+++ forge.config.ts
@@ -110,6 +110,8 @@ const config: ForgeConfig = {
             }),
           },
           osxNotarize: {
+            // https://forums.developer.apple.com/forums/thread/128772
+            hardenedRuntime: true,
             appleId: process.env.APPLE_ID,
             appleIdPassword: process.env.APPLE_PASSWORD,
             teamId: process.env.APPLE_TEAM_ID,
