bdeec824e352ebd1943406099df50b99b6a7d692
jinxin001 <jinxin001@bytedance.com>
2025-01-31 15:37:46 +0800

    refactor(shared): only export @ui-tars/shared/{part} namespace

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,8 +1,8 @@
-/**
+/*
  * Copyright (c) 2025 Bytedance, Inc. and its affiliates.
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from './agent';
-export * from './data';
-export * from './share';
+export * from './coords';
+export * from './format';
+export { default as sleep } from './sleep';
