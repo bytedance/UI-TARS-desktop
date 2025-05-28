eb0f228ab5efa13c93878d3f6bbd6d29624f4544
赵庆浩 <zhaoqinghao@bytedance.com>
2025-05-28 21:26:11 +0800

    fix(browser-operator): wait load event when goto url

--- packages/ui-tars/operators/browser-operator/src/browser-operator.ts
+++ packages/ui-tars/operators/browser-operator/src/browser-operator.ts
@@ -563,7 +563,7 @@ export class BrowserOperator extends Operator {
 
     this.logger.info(`Navigating to: ${url}`);
     await page.goto(url, {
-      waitUntil: 'networkidle0',
+      waitUntil: 'load',
     });
     this.logger.info('Navigation completed');
   }
