0bc9335823383e3fce2f0ffd2cae451675a20e6a
skychx <skychx@hotmail.com>
2025-06-10 21:52:06 +0800

    feat(ui-tars): add drag

--- apps/ui-tars/src/renderer/src/pages/home/index.tsx
+++ apps/ui-tars/src/renderer/src/pages/home/index.tsx
@@ -38,6 +38,7 @@ import computerUseImg from '@resources/home_img/computer_use.png?url';
 import browserUseImg from '@resources/home_img/browser_use.png?url';
 import { sleep } from '@ui-tars/shared/utils';
 import { FreeTrialDialog } from '../../components/AlertDialog/freeTrialDialog';
+import { DragArea } from '../../components/Common/drag';
 
 const FreeButton = ({
   onClick,
@@ -264,90 +265,94 @@ const Home = () => {
   // };
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <h1 className="text-2xl font-semibold mt-1 mb-12">
-        Welcome to UI-TARS Desktop
-      </h1>
-      <div className="flex gap-6">
-        <Card className="w-[400px] py-5">
-          <CardHeader className="px-5">
-            <CardTitle>Computer Operator</CardTitle>
-            <CardDescription>
-              Use the UI-TARS model to automate and complete tasks directly on
-              your computer with AI assistance.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="px-5">
-            <img
-              src={computerUseImg}
-              alt=""
-              className="w-full h-full aspect-video object-fill rounded-lg"
-            />
-          </CardContent>
-          <CardFooter className="gap-3 px-5 flex justify-between">
-            {/* {renderRemoteComputerButton()} */}
-            <FreeButton
-              onClick={() => handleRemotePress(Operator.RemoteComputer)}
-            >
-              Use Remote Computer
-            </FreeButton>
-            <Button
-              onClick={() => handleLocalPress(Operator.LocalComputer)}
-              variant="secondary"
-              className="flex-1"
-            >
-              Use Local Computer
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className="w-[400px] py-5">
-          <CardHeader className="px-5">
-            <CardTitle>Browser Operator</CardTitle>
-            <CardDescription>
-              Let the UI-TARS model help you automate browser tasks, from
-              navigating pages to filling out forms.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="px-5">
-            <img
-              src={browserUseImg}
-              alt=""
-              className="w-full h-full aspect-video object-fill rounded-lg"
-            />
-          </CardContent>
-          <CardFooter className="gap-3 px-5 flex justify-between">
-            {/* {renderRemoteBrowserButton()} */}
-            <FreeButton
-              onClick={() => handleRemotePress(Operator.RemoteBrowser)}
-            >
-              Use Remote Browser
-            </FreeButton>
-            <Button
-              onClick={() => handleLocalPress(Operator.LocalBrowser)}
-              variant="secondary"
-              className="flex-1"
-            >
-              Use Local Browser
-            </Button>
-          </CardFooter>
-        </Card>
+    <div className="w-full h-full flex flex-col">
+      <DragArea></DragArea>
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-semibold mt-1 mb-12">
+          Welcome to UI-TARS Desktop
+        </h1>
+        <div className="flex gap-6">
+          <Card className="w-[400px] py-5">
+            <CardHeader className="px-5">
+              <CardTitle>Computer Operator</CardTitle>
+              <CardDescription>
+                Use the UI-TARS model to automate and complete tasks directly on
+                your computer with AI assistance.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="px-5">
+              <img
+                src={computerUseImg}
+                alt=""
+                className="w-full h-full aspect-video object-fill rounded-lg"
+              />
+            </CardContent>
+            <CardFooter className="gap-3 px-5 flex justify-between">
+              {/* {renderRemoteComputerButton()} */}
+              <FreeButton
+                onClick={() => handleRemotePress(Operator.RemoteComputer)}
+              >
+                Use Remote Computer
+              </FreeButton>
+              <Button
+                onClick={() => handleLocalPress(Operator.LocalComputer)}
+                variant="secondary"
+                className="flex-1"
+              >
+                Use Local Computer
+              </Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[400px] py-5">
+            <CardHeader className="px-5">
+              <CardTitle>Browser Operator</CardTitle>
+              <CardDescription>
+                Let the UI-TARS model help you automate browser tasks, from
+                navigating pages to filling out forms.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="px-5">
+              <img
+                src={browserUseImg}
+                alt=""
+                className="w-full h-full aspect-video object-fill rounded-lg"
+              />
+            </CardContent>
+            <CardFooter className="gap-3 px-5 flex justify-between">
+              {/* {renderRemoteBrowserButton()} */}
+              <FreeButton
+                onClick={() => handleRemotePress(Operator.RemoteBrowser)}
+              >
+                Use Remote Browser
+              </FreeButton>
+              <Button
+                onClick={() => handleLocalPress(Operator.LocalBrowser)}
+                variant="secondary"
+                className="flex-1"
+              >
+                Use Local Browser
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+        <LocalSettingsDialog
+          isOpen={localConfig.open}
+          onSubmit={handleLocalSettingsSubmit}
+          onClose={handleLocalSettingsClose}
+        />
+        {/* <RemoteSettingsDialog
+          isOpen={remoteConfig.open}
+          operator={remoteConfig.operator}
+          onSubmit={handleReomteSettingsSubmit}
+          onClose={handleRemoteSettingsClose}
+        /> */}
+        <FreeTrialDialog
+          open={remoteConfig.open}
+          onOpenChange={handleRemoteDialogClose}
+          onConfirm={handleFreeDialogComfirm}
+        />
       </div>
-      <LocalSettingsDialog
-        isOpen={localConfig.open}
-        onSubmit={handleLocalSettingsSubmit}
-        onClose={handleLocalSettingsClose}
-      />
-      {/* <RemoteSettingsDialog
-        isOpen={remoteConfig.open}
-        operator={remoteConfig.operator}
-        onSubmit={handleReomteSettingsSubmit}
-        onClose={handleRemoteSettingsClose}
-      /> */}
-      <FreeTrialDialog
-        open={remoteConfig.open}
-        onOpenChange={handleRemoteDialogClose}
-        onConfirm={handleFreeDialogComfirm}
-      />
+      <DragArea></DragArea>
     </div>
   );
 };
