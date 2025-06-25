d27a4945925e972461abac184c8f203caa114cca
chenhaoli <chenhaoli@bytedance.com>
2025-06-25 19:37:35 +0800

    docs: handle localed path

--- multimodal/websites/docs/src/components/QuickStartActionCard.tsx
+++ multimodal/websites/docs/src/components/QuickStartActionCard.tsx
@@ -1,28 +1,26 @@
-import { usePageData, useI18n } from 'rspress/runtime';
-import React from 'react';
-import './QuickStartActionCard.css';
+import { useI18n } from 'rspress/runtime';
 import { ActionCard } from './ActionCard';
 import { ActionCardContainer } from './ActionCardContainer';
+import { useLocaledPath } from './hooks';
+import './QuickStartActionCard.css';
 
 export const QuickStartActionCard = () => {
-  const {
-    siteData: { base },
-  } = usePageData();
   const t = useI18n<typeof import('i18n')>();
+  const { localedPath } = useLocaledPath();
 
   const cards = [
     {
       title: t('quick-action-card.cli.title'),
       description: t('quick-action-card.cli.description'),
       icon: '⌨️',
-      href: `${base}guide/get-started/quick-start.html`,
+      href: `${localedPath}guide/get-started/quick-start.html`,
       color: 'green',
     },
     {
       title: t('quick-action-card.sdk.title'),
       description: t('quick-action-card.sdk.description'),
       icon: '📦',
-      href: `${base}sdk/introduction.html`,
+      href: `${localedPath}sdk/introduction.html`,
       color: 'purple',
     },
   ];
