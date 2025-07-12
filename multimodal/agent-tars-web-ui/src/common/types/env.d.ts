e187b331df2066fcd19945135395c26f129b6a02
chenhaoli <chenhaoli@bytedance.com>
2025-07-12 23:53:47 +0800

    chore: tweaks

--- multimodal/agent-tars-web-ui/src/common/types/env.d.ts
+++ multimodal/agent-tars-web-ui/src/common/types/env.d.ts
@@ -20,7 +20,6 @@ declare global {
     AGENT_TARS_REPLAY_MODE?: boolean;
     AGENT_TARS_SESSION_DATA?: SessionMetadata;
     AGENT_TARS_EVENT_STREAM?: AgentEventStream.Event[];
-    AGENT_TARS_MODEL_INFO?: { provider: string; model: string };
     AGENT_TARS_VERSION_INFO?: AgentTARSServerVersionInfo;
   }
 }
