c0338461a4378863c9706e4c5eb04dcde0376f02
chenhaoli <chenhaoli@bytedance.com>
2025-07-13 21:43:17 +0800

    fix(agent-kernel): update kor prompt

--- multimodal/agent/src/tool-call-engine/PromptEngineeringToolCallEngine.ts
+++ multimodal/agent/src/tool-call-engine/PromptEngineeringToolCallEngine.ts
@@ -99,14 +99,13 @@ ${JSON.stringify(schema)}
     return `${instructions}
 
 <tool_instruction>
-  You have access to the following tools:
+  1. You have access to the following tools:
 
   <available_tools>
   ${toolsDescription}
   </available_tools>
 
-  To use a tool, your response MUST use the following format, you need to ensure that it is a valid JSON string matches the Parameters JSON Schema:
-  IMPORTANT: You can always ONLY call tools mentioned in available_tools
+  2. To use a tool, your response MUST use the following format, you need to ensure that it is a valid JSON string matches the Parameters JSON Schema:
 
   <tool_call>
   {
@@ -118,9 +117,11 @@ ${JSON.stringify(schema)}
   }
   </tool_call>
 
-  If you want to provide a final answer without using tools, respond in a conversational manner WITHOUT using the tool_call format.
-
-  When you receive tool results, they will be provided in a user message. Use these results to continue your reasoning or provide a final answer.
+  3. If you want to provide a final answer without using tools, respond in a conversational manner WITHOUT using the tool_call format.
+  4. WARNING:
+    4.1. You can always ONLY call tools mentioned in <available_tools>
+    4.2. After outputting </tool_call>, you MUST STOP immediately and wait for the tool result in the next agent loop. DO NOT generate any additional text.
+    4.3. When you receive tool results, they will be provided in a user message. Use these results to continue your reasoning or provide a final answer.
 </tool_instruction>
 `;
   }
@@ -136,6 +137,7 @@ ${JSON.stringify(schema)}
       messages,
       temperature,
       stream: false,
+      // stop: '</tool_call>',
     };
   }
 
