5711cda9f187f7b3522b2fe415b6bf7f27bb26c3
chenhaoli <chenhaoli@bytedance.com>
2025-08-21 16:03:29 +0800

    Revert "feat(omni-tars): move `webui` config to agent"
    This reverts commit 64f86f20974042cfd0791fadfdc236e3de005f31.

--- multimodal/omni-tars/omni-agent/src/index.ts
+++ multimodal/omni-tars/omni-agent/src/index.ts
@@ -6,8 +6,7 @@ import { codePlugin, CodeToolCallEngineProvider } from '@omni-tars/code-agent';
 import { mcpPlugin, McpToolCallEngineProvider } from '@omni-tars/mcp-agent';
 import { guiPlugin, GuiToolCallEngineProvider } from '@omni-tars/gui-agent';
 import { ComposableAgent, createComposableToolCallEngineFactory } from '@omni-tars/core';
-import type { AgentAppConfig } from '@tarko/interface';
-import { deepMerge } from '@tarko/shared-utils';
+import { AgentOptions } from '@tarko/agent';
 
 const toolCallEngine = createComposableToolCallEngineFactory({
   engines: [
@@ -17,51 +16,15 @@ const toolCallEngine = createComposableToolCallEngineFactory({
   ],
 });
 
-const sandboxUrl = process.env.AIO_SANDBOX_URL;
-
-const DEFAULT_OPTIONS: AgentAppConfig = {
-  maxTokens: 32768,
-  webui: {
-    logo: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/zyha-aulnh/ljhwZthlaukjlkulzlp/appicon.png',
-    title: 'Omni-TARS Agent',
-    subtitle: 'Offering seamless integration with a wide range of real-world tools.',
-    welcomTitle: 'An multimodal AI agent',
-    welcomePrompts: [
-      'Search for the latest GUI Agent papers',
-      'Find information about UI TARS',
-      'Tell me the top 5 most popular projects on ProductHunt today',
-      'write a tic-tac-toe program in js',
-      'Write hello world using python',
-      'Use jupyter to calculate who is greater in 9.11 and 9.9',
-      'Write a python code to download the paper https://arxiv.org/abs/2505.12370, and convert the pdf to markdown',
-    ],
-    workspace: {
-      panels: [
-        {
-          title: 'Code Server',
-          panel: sandboxUrl + '/code-server/',
-        },
-        {
-          title: 'VNC',
-          panel: sandboxUrl + '/vnc/index.html',
-        },
-      ],
-    },
-  },
-  toolCallEngine,
-  share: {
-    provider: process.env.SHARE_PROVIDER,
-  },
-};
-
 export default class OmniTARSAgent extends ComposableAgent {
   static label = 'Omni-TARS Agent';
 
-  constructor(options: AgentAppConfig) {
-    const mergedOptions = deepMerge(DEFAULT_OPTIONS, options ?? {});
+  constructor(options: AgentOptions) {
     super({
-      ...mergedOptions,
+      ...options,
       plugins: [mcpPlugin, guiPlugin, codePlugin],
+      toolCallEngine,
+      maxTokens: 32768,
     });
   }
 }
