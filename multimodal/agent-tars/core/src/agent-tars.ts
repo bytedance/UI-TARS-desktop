3c513f8586b50a89544c8f0dbba7e44f8ac59f64
chenhaoli <chenhaoli@bytedance.com>
2025-08-29 22:57:25 +0800

    chore: default config

--- multimodal/agent-tars/core/src/agent-tars.ts
+++ multimodal/agent-tars/core/src/agent-tars.ts
@@ -64,7 +64,7 @@ export class AgentTARS<T extends AgentTARSOptions = AgentTARSOptions> extends MC
     ],
     enableContextualSelector: true,
     guiAgent: {
-      defaultScreenshotRenderStrategy: 'both',
+      defaultScreenshotRenderStrategy: 'beforeAction',
       enableScreenshotRenderStrategySwitch: true,
       renderGUIAction: true,
     },
