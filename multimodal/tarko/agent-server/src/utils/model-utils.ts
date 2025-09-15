3e8c12659f7812c964e85ff955aa8e03e694bf86
chenhaoli <chenhaoli@bytedance.com>
2025-09-15 17:52:08 +0800

    chore: code format

--- multimodal/tarko/agent-server/src/utils/model-utils.ts
+++ multimodal/tarko/agent-server/src/utils/model-utils.ts
@@ -6,13 +6,16 @@
 import type { AgentModel, AgentAppConfig } from '../types';
 
 export function getAvailableModels(appConfig: AgentAppConfig): AgentModel[] {
-  const allModels = [...(appConfig.model ? [appConfig.model] : []), ...(appConfig.server?.models || [])];
-  
+  const allModels = [
+    ...(appConfig.model ? [appConfig.model] : []),
+    ...(appConfig.server?.models || []),
+  ];
+
   // Deduplicate by model.id, keeping the first occurrence
-  const uniqueModels = allModels.filter((model, index, arr) => 
-    arr.findIndex(m => m.id === model.id) === index
+  const uniqueModels = allModels.filter(
+    (model, index, arr) => arr.findIndex((m) => m.id === model.id) === index,
   );
-  
+
   return uniqueModels;
 }
 
