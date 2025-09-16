bb581a52fb2c1a0f4013fc43660357807f2167fb
chenhaoli <chenhaoli@bytedance.com>
2025-09-16 20:34:03 +0800

    fix(tarko-ui): update component exports for new directory structure

--- multimodal/tarko/ui/src/components/index.ts
+++ multimodal/tarko/ui/src/components/index.ts
@@ -1,3 +1,19 @@
 export * from './code-editor';
-export * from './mui';
-export * from './dialog';
+export * from './Dialog';
+export * from './Tooltip';
+
+// Direct re-exports from @mui/material
+export {
+  Box,
+  Typography,
+  CircularProgress,
+  Select,
+  MenuItem,
+  FormControl,
+  Menu,
+  IconButton,
+  Divider
+} from '@mui/material';
+
+export { ThemeProvider } from '@mui/material/styles';
+export * from '../utils/mui';
