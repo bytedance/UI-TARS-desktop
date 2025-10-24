6640fd2f0df67628c8a7393de612da434e100624
Abubakar-1214 <onlyusemywork@gmail.com>
2025-10-24 19:26:39 +0500

    Delete index.ts

--- multimodal/tarko/agent-ui/src/standalone/chat/MessageInput/index.ts
+++
@@ -1,4 +0,0 @@
-export { MessageAttachments } from './MessageAttachments';
-export { ChatInput } from './ChatInput';
-export { ImagePreviewInline } from './ImagePreviewInline';
-export { composeMessageContent, isMessageEmpty, parseContextualReferences } from './utils';
