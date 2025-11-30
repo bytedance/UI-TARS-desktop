6b04dd4faf56c795f6d94fefac3583c097445ec0
Ruipeng Wang <66009386+Chilly-RP@users.noreply.github.com>
2025-11-30 21:15:52 +0800

    style:修改了背景透明度

--- apps/ui-tars/src/renderer/src/pages/widget/index.tsx
+++ apps/ui-tars/src/renderer/src/pages/widget/index.tsx
@@ -175,7 +175,7 @@ const Widget = () => {
   return (
     <div
       className="w-100 h-70 overflow-hidden p-4 bg-white/90 dark:bg-gray-800/90 rounded-[10px] border-gray-300 fixed bottom-0 left-0 right-0 pointer-events-none select-none flex flex-col"
-      style={{ borderWidth: isWin ? '1px' : '0', opacity: 0.75 }}
+      style={{ borderWidth: isWin ? '1px' : '0', opacity: 0.95 }}
     >
       <div className="flex draggable-area pointer-events-none">
         {/* Logo */}
